d6e703409198dd79421dab25bd9b63001dd8547b
Artsiom <novitskii.a@yandex.ru>
2018-04-01 15:37:10 +0300

    Урок 1 - Порвое ДЗ

---
+++ Lesson 1/js/script.js
@@ -0,0 +1,25 @@
+let	money = +prompt('Ваш бюджет на месяц?');
+let shopName = prompt('Название вашего магазина?');
+let shopGoods = [prodOne = prompt('Что будем продавать?'),
+			 prodTwo = prompt('Что будем продавать?'),
+			 prodTree = prompt('Что будем продавать?')];
+employers = {
+	employer1: 'Jhon',
+	employer2: 'Bob',
+	employer3: "Bill",
+};
+
+let win = window.open();
+
+mainList = {
+	money,
+	shopName,
+	shopGoods,
+	employers,	
+	Open: true
+}
+
+console.log(mainList);
+let = moneyDay = money / 30;
+console.log(moneyDay);
+alert('Ваш бюджет на день: ' + moneyDay);
