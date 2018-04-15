20ba61d9e0e6a1270517afcda4f8383b8d2614d6
Artsiom <novitskii.a@yandex.ru>
2018-04-15 23:45:07 +0300

    Выполнен 8 урок

---
+++ Lesson 8/js/script.js
@@ -0,0 +1,93 @@
+window.addEventListener('DOMContentLoaded', () => {
+	let tab = document.getElementsByClassName("info-header-tab"),
+		info = document.querySelector('.info-header'),
+		tabContent = document.getElementsByClassName('info-tabcontent');
+
+	function hideTabContent (a) {
+			for (i = a; i < tabContent.length; i++) {
+				tabContent[i].classList.remove('show');
+				tabContent[i].classList.add('hide');
+			}
+		};
+
+	hideTabContent (1);
+
+	function showTabContent (b) {
+		if (tabContent[b].classList.contains('hide')) {
+			hideTabContent(0);
+			tabContent[b].classList.remove('hide');
+			tabContent[b].classList.add('show');
+
+		}
+	};
+
+	info.addEventListener('click', function (event) {
+		let target = event.target;
+		if (target.className == 'info-header-tab') {
+			for (i = 0; i < tab.length; i++) {
+				if (target == tab[i]) {
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+
+});
+
+// Timer
+
+let deadLine = '2018-04-17';
+
+function getTimeRemaining (endtime) {
+	let t = Date.parse(endtime) - Date.parse(new Date()),
+		seconds = Math.floor( (t / 1000) % 60),
+		minutes = Math.floor( (t / 1000 / 60) % 60),
+		hours = Math.floor( (t / (1000 * 60 * 60)) );
+
+	return {
+		'total': t,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+};	
+
+function setTime (id, endtime) {
+	let timer = document.getElementById(id),
+		hours = timer.querySelector('.hours'),
+		minutes = timer.querySelector('.minutes'),
+		seconds = timer.querySelector('.seconds');
+
+		function updateClock () {
+			let t = getTimeRemaining(endtime);
+			if (t.hours < 10) {
+			seconds.innerHTML = '0' + t.hours;
+			} else {
+				seconds.innerHTML = t.hours;
+			};
+
+			if (t.minutes < 10) {
+			seconds.innerHTML = '0' + t.minutes;
+			} else {
+				seconds.innerHTML = t.minutes;
+			};
+			
+			if (t.seconds < 10) {
+			seconds.innerHTML = '0' + t.seconds;
+			} else {
+				seconds.innerHTML = t.seconds;
+			};
+			
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		};
+
+		updateClock();
+		let timeInterval = setInterval(updateClock, 1000);
+
+};
+
+setTime ('timer', deadLine);
